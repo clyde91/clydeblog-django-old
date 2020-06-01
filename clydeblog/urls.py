25ded277fa71ab1667dd78af65fabd218a32b31d
clyde <clyde91@163.com>
2020-06-01 19:42:59 +0800

    0.91e
    git commit -m 0.91e

--- clydeblog/urls.py
+++ clydeblog/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('architecture/', include('architecture.urls')),
     path('', views.index, name='home'),
     path('test', views.test, name='test'),
-]
+] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+
