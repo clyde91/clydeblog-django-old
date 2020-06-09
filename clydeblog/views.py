c7814d663850a1c5ed37c65c19444789fdf21fc7
clyde <clyde91@163.com>
2020-06-10 01:14:09 +0800

    0.93b

--- clydeblog/views.py
+++ clydeblog/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404, redirect
 from django.contrib import auth
+from django.urls import reverse    # 反向解析
 
 
 def index(request):
@@ -21,9 +22,10 @@ def login(request):
     username = request.POST['username']     # POST大写
     password = request.POST['password']
     user = auth.authenticate(request, username=username,password=password)
+    referer = request.META.get('HTTP_REFERER', referer('home'))    # 获得跳转前的网站。如果没有则后者  referer反向解析home在的地址
     if user is not None:
         auth.login(request, user)
-        return redirect('/')
+        return redirect(referer)
     else:
         return render(request, 'error.html', {"message":'用户名或密码不正确'})
 
